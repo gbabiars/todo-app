5bf07f7958dbf5bddfbd13ec15425d7785704c3c
Greg Babiars <greg.babiars@keap.com>
2025-04-11 16:12:14 -0700

    Style checkbox to match design specs

--- src/components/TaskListItem.tsx
+++ src/components/TaskListItem.tsx
@@ -1,3 +1,6 @@
+import { Checkbox } from "radix-ui";
+import { CheckIcon } from "@radix-ui/react-icons";
+
 interface TaskListItemProps {
   id: string;
   description: string;
@@ -14,10 +17,21 @@ function TaskListItem({
   onComplete,
 }: TaskListItemProps) {
   return (
-    <div className="flex items-center justify-between bg-neutral py-2.5 px-4">
+    <div className="flex items-center justify-between bg-neutral py-[5px] px-3">
       <div className="flex items-center gap-3">
-        <input type="checkbox" checked={isComplete} onChange={onComplete} />
-        <p className="text-lg">{description}</p>
+        <Checkbox.Root
+          aria-labelledby={`task-${id}`}
+          checked={isComplete}
+          onCheckedChange={onComplete}
+          className="group flex items-center justify-center h-3 w-3 rounded-xs border border-black active:bg-black data-[state=checked]:bg-black transition-all"
+        >
+          <Checkbox.Indicator>
+            <CheckIcon className="h-3 w-3 text-white group-active:opacity-0 transition-all" />
+          </Checkbox.Indicator>
+        </Checkbox.Root>
+        <p id={`task-${id}`} className="text-lg">
+          {description}
+        </p>
       </div>
     </div>
   );

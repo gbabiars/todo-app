8cfc418cc910393ee094b30681d1db5b2d07a97b
Greg Babiars <greg.babiars@keap.com>
2025-04-12 07:00:29 -0700

    Add patch for updating task complete status
    This adds a patch call when the complete status is toggled.  This will update both complete and incomplete.  This optimistically updates the UI and will warn and revert if the update was unsuccessful.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,11 +35,54 @@ export default function TasksPage() {
       });
   }, []);
 
+  function handleUpdateError(taskId: string) {
+    setTasks((prevTasks) => {
+      return prevTasks.map((t) =>
+        t.id === taskId ? { ...t, isComplete: !t.isComplete } : t
+      );
+    });
+  }
+
+  function handleComplete(taskId: string) {
+    const task = tasks.find((task) => task.id === taskId);
+
+    if (!task) {
+      return;
+    }
+
+    // Optimistically update the task list and we will alert the user if the update fails
+    setTasks((prevTasks) => {
+      return prevTasks.map((t) =>
+        t.id === taskId ? { ...t, isComplete: !t.isComplete } : t
+      );
+    });
+
+    fetch(process.env.NEXT_PUBLIC_API_URL + "/patch/" + taskId, {
+      method: "PATCH",
+      headers: {
+        "X-Api-Key": process.env.NEXT_PUBLIC_API_KEY as string,
+      },
+      body: JSON.stringify({
+        isComplete: !task.isComplete,
+      }),
+    })
+      .then((res) => res.json())
+      .then(({ status }) => {
+        if (status !== "success") {
+          handleUpdateError(taskId);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        handleUpdateError(taskId);
+      });
+  }
+
   return (
     <main className="w-full max-w-xl mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Tasks</h1>
 
-      <Tasks tasks={tasks} status={status} onComplete={() => {}} />
+      <Tasks tasks={tasks} status={status} onComplete={handleComplete} />
     </main>
   );
 }
